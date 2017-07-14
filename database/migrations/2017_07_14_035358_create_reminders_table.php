2bbcb890beb06810cd14bc35a80a96f7265527f4
drevel <drevel@opensky.com>
2017-07-13 23:59:07 -0700

    rough CRUD

---
+++ database/migrations/2017_07_14_035358_create_reminders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRemindersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user');
+            $table->dateTime('when');
+            $table->text('reminder');
+            $table->dateTime('remindedAt')->nullable();
+            $table->dateTime('canceledAt')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reminders');
+    }
+}
